7ce85809e2742f1471b073dc2819a49dc836b6c8
Lucretia Teretto <lteretto@asu.edu>
2025-06-22 03:18:48 +0000

    added random function

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 
+// This function randomly selects Rock, Paper, or Scissors for the computer
+function getComputerChoice() {
+  // Generate a random number: 0, 1, or 2
+  const randomNumber = Math.floor(Math.random() * 3);
+  // Use if statements to pick a choice based on the random number
+  if (randomNumber === 0) {
+    return "Rock";
+  }
+  if (randomNumber === 1) {
+    return "Paper";
+  }
+  // If it's not 0 or 1, it must be 2
+  return "Scissors";
+}
+
 // Add event listener for rock button
 rockButton.addEventListener("click", function() {
   console.log("You chose: Rock 🪨");
